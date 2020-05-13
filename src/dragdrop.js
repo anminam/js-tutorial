b5f09293b2e0ae4e43f877b4148deda808bda94b
Anminam <minamhunmin@gmail.com>
2020-05-13 15:56:21 +0900

    update drop

--- src/dragdrop.js
+++ src/dragdrop.js
@@ -1,40 +1,64 @@
-const fill = document.querySelector('.fill');
-const empties = document.querySelectorAll('.empty');
+const items = document.querySelectorAll('.items .item');
+const dropzones = document.querySelectorAll('.dropzone');
+console.log(items, dropzones);
 
+let selectedItem = null;
 
-fill.addEventListener('dragstart', dragStart);
-fill.addEventListener('dragend', dragEnd);
+// itemss.forEach(function(item) {
 
-for(const empty of empties) {
-    empty.addEventListener('dragover', dragOver);
-    empty.addEventListener('dragenter', dragEnter);
-    empty.addEventListener('dragleave', dragLeave);
-    empty.addEventListener('drop', dragDrop);
-}
+//     item.addEventListener()
+// });
 
-function dragStart(){
-    console.log('start');
-    this.className += ' hold';
-    setTimeout(() => (this.className = 'invisible', 0));
-}
-function dragEnd() {
-    this.className = 'fill';
-}
+dropzones.forEach(function(zone){
+    zone.addEventListener('dragstart', function(e) {
+        e.dataTransfer.setData('id',e.target.id);
+        // console.log('dragstart');
+    });
 
-function dragOver (e) {
-    e.preventDefault();
-    console.log('dragOver');
-}
-function dragEnter (e) {
-    e.preventDefault();
-    this.className += ' hover';
-    console.log('dragEnter');
-}
-function dragLeave () {
-    this.className = 'empty';
-    console.log('dragLeave');
-}
-function dragDrop () {
-    this.className = 'empty';
-    this.append(fill);
+    zone.addEventListener('dragenter', function(e) {
+
+        console.log('enter',e)
+        // if(!this.querySelector('.temp-item')) {
+            const tempItem = document.createElement('li');
+            tempItem.classList.add('item');
+            tempItem.classList.add('temp-item');
+            this.querySelector('ul').append(tempItem);
+        // }
+    },false);
+
+    zone.addEventListener('dragleave', function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+
+        console.log(e, e.fromElement);
+        if(!e.fromElement.className.includes('dropzone')) {
+            return false;
+        }
+
+        removeTempItem(this);
+
+    }, false);
+    zone.addEventListener('dragover', function(e) {
+        const { className } = e.target;
+        
+        
+    }, false);
+
+    zone.addEventListener('drop', function(e,a) {
+        e.stopPropagation();
+        e.preventDefault();
+        
+        const id = e.dataTransfer.getData('id');
+        const item = document.getElementById(id);
+        this.querySelector('ul').append(item);
+
+    }, false);
+
+});
+
+const removeTempItem = (el) => {
+    let tempItem = el.querySelector('.temp-item');
+    if (tempItem) {
+        tempItem.remove();
+    }
 }
