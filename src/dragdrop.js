5090a042cd37be90e8749386e0c6bc528e4285dc
Anminam <minamhunmin@gmail.com>
2020-05-13 16:58:03 +0900

    update dropbox

--- src/dragdrop.js
+++ src/dragdrop.js
@@ -3,62 +3,59 @@ const dropzones = document.querySelectorAll('.dropzone');
 console.log(items, dropzones);
 
 let selectedItem = null;
+const HOVERED = 'hovered';
 
-// itemss.forEach(function(item) {
-
-//     item.addEventListener()
-// });
-
-dropzones.forEach(function(zone){
+dropzones.forEach(function(zone) {
     zone.addEventListener('dragstart', function(e) {
         e.dataTransfer.setData('id',e.target.id);
-        // console.log('dragstart');
     });
 
-    zone.addEventListener('dragenter', function(e) {
-
-        console.log('enter',e)
-        // if(!this.querySelector('.temp-item')) {
-            const tempItem = document.createElement('li');
-            tempItem.classList.add('item');
-            tempItem.classList.add('temp-item');
-            this.querySelector('ul').append(tempItem);
-        // }
-    },false);
-
     zone.addEventListener('dragleave', function(e) {
-        e.stopPropagation();
-        e.preventDefault();
 
-        console.log(e, e.fromElement);
-        if(!e.fromElement.className.includes('dropzone')) {
+        const {target} = e;
+        if (this == target) {
+            this.classList.remove(HOVERED);
+        }
+        
+
+        if (target.className.includes('dropzone')) {
+            return true;
+        } else {
             return false;
         }
 
-        removeTempItem(this);
+    });
 
-    }, false);
     zone.addEventListener('dragover', function(e) {
-        const { className } = e.target;
-        
-        
-    }, false);
-
-    zone.addEventListener('drop', function(e,a) {
-        e.stopPropagation();
         e.preventDefault();
+
+        const { target } = e;
         
+        // allRemoveHover();
+        this.classList.add(HOVERED);
+        
+
+        if (target.className.includes('dropzone')) {
+            return true;
+        } else {
+            return false;
+        }
+
+    });
+
+    zone.addEventListener('drop', function(e) {
+        
+        allRemoveHover();
         const id = e.dataTransfer.getData('id');
         const item = document.getElementById(id);
         this.querySelector('ul').append(item);
 
-    }, false);
+    });
 
 });
 
-const removeTempItem = (el) => {
-    let tempItem = el.querySelector('.temp-item');
-    if (tempItem) {
-        tempItem.remove();
-    }
+const allRemoveHover = () => {
+    dropzones.forEach(function(zone) {
+        zone.classList.remove(HOVERED);
+    })
 }
